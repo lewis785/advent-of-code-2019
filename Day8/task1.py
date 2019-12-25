ca9fb4bb08203655df79c71593b90e21e3952d36
Lewis McNeill <lewisfmcn@yahoo.co.uk>
2019-12-25 17:54:11 +0000

    Completed Day 8
    * completed task 1
    
    * completed task 2

---
+++ Day8/task1.py
@@ -0,0 +1,28 @@
+def image_size(width, height):
+    return width * height
+
+
+def calculate(layer_values, layer_size):
+    layer_with_least_zeros = None
+    for lay_count in range(int(len(layer_values) / layer_size)):
+        index = lay_count * layer_size
+        current_layer = layer_values[index: index + layer_size]
+
+        if layer_with_least_zeros is None:
+            layer_with_least_zeros = current_layer
+
+        if current_layer.count('0') < layer_with_least_zeros.count('0'):
+            layer_with_least_zeros = current_layer
+
+    return layer_with_least_zeros.count('1') * layer_with_least_zeros.count('2')
+
+
+def main():
+    with open('input.txt') as inputFile:
+        layer_values = list(inputFile.readline().rstrip('\n'))
+
+    print (calculate(layer_values, image_size(25, 6)))
+
+
+if __name__ == '__main__':
+    main()
